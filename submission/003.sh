57bd735b1793ce608940922b830e88dd2b7b3334
Matheus Duarte <113726352+matheusduartedm@users.noreply.github.com>
2024-11-20 15:31:17 -0300

    Update 003.sh

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,5 @@
 # How many new outputs were created by block 123,456?
+#!/bin/bash
+blockhash=$(bitcoin-cli getblockhash 123456)
+block=$(bitcoin-cli getblock $blockhash 2)
+echo "$block" | jq '[.tx[].vout[]] | length'
