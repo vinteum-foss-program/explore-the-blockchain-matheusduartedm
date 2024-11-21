8d2af68fc13fc950f4bf840b6d4cbf43927c39dd
Matheus Duarte <113726352+matheusduartedm@users.noreply.github.com>
2024-11-21 19:17:29 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,19 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+#!/bin/bash
+
+block_hash=$(bitcoin-cli getblockhash 123321)
+
+txids=$(bitcoin-cli getblock $block_hash | jq -r '.tx[]')
+
+for txid in $txids; do
+    decoded_tx=$(bitcoin-cli getrawtransaction $txid true)
+    vout_indices=$(echo $decoded_tx | jq '.vout[].n')
+    for vout in $vout_indices; do
+        utxo=$(bitcoin-cli gettxout $txid $vout)
+        if [ -n "$utxo" ]; then
+            address=$(echo $utxo | jq -r '.scriptPubKey.address')
+            echo $address
+            exit 0
+        fi
+    done
+done
